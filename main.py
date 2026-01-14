0de51c80c6b03d8161733040f2c73e75d40302c3
akberelci <akberelc10@gmail.com>
2026-01-14 12:06:02 +0000

    gameclass

---
+++ main.py
@@ -0,0 +1,15 @@
+class Game:
+    def __init__(self,name,gametype):
+        self.name = name
+        self.gametype = gametype
+g1 = Game('eafc26','soccer')
+print(g1.name , g1.gametype)
+
+number = 1
+if number > 0 :
+    print('positive')
+elif number < 0 :
+        print('negaitve')
+else :
+        print('equal')
+
